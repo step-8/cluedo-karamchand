d4ac6463242330d52a60ef4ccadd1a052d8da75d
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-08-06 10:17:57 +0530

    [#1] Prem|Roshan -- Configured env variables and git commit template

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,4 +2,13 @@
 
 touch TODO.md;
 
-npm install;
+echo '==>Installing npm packages';
+npm install &> /dev/null;
+
+echo -e '==>Setting git commit template locally';
+git config --local commit.template .github/commit_template;
+
+echo -e "==>Configuring environment variables"
+echo -e "ENV=\"PRODUCTION\"" > .env
+
+echo 'Setup done';
---
+++ bin/testSetup.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+echo -e "ENV=\"DEVELOPMENT\"" > .env
