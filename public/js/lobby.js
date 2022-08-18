a71c7d2e8f9fe8624de528d4f83094d29567495f
Sakshi Jagtap <99532147+sakshikjagtap@users.noreply.github.com>
2022-08-18 21:15:42 +0530

    [#15] Sakshi/Prajakta -- Displayed lobby status and username in lobby

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -10,7 +10,8 @@ const sendRequest = (method, path, content, cb) => {
 const playerHtml = player => {
   const character = player.character;
   return generateHTML(['div', { id: character, className: 'character' },
-    ['img', { className: 'logo', src: `/images/${character}.png` }, ''],
+    ['div', { className: 'character-img' },
+      ['img', { src: `/images/${character}.png` }, '']],
     ['div', { className: 'player-name' }, player.name]]);
 };
 
@@ -20,23 +21,34 @@ const addPlayers = ({ players, maxPlayers }) => {
   playersElement.replaceChildren(...playerList);
 
   if (maxPlayers === playerList.length) {
-    document.querySelector('form').submit();
+    document.querySelector('#start-game').click();
   }
 };
 
 const generateLobby = () => {
   sendRequest('GET', '/api/game', '', (xhr) => {
     const game = JSON.parse(xhr.response);
+
+    const username = game.you.name;
+    document.querySelector('.username').innerText = `Hey ${username}!`;
+
     const roomId = 'Room ID: ' + game.gameId;
     document.querySelector('.room-id').replaceChildren(roomId);
     addPlayers(game);
   });
 };
 
+const updateStatus = ({ players, maxPlayers }) => {
+  const restOfPlayers = maxPlayers - players.length;
+  const statusEle = document.querySelector('.status');
+  statusEle.innerText = `Waiting for ${restOfPlayers} players...`;
+};
+
 const updateLobby = () => {
   sendRequest('GET', '/api/game', '', (xhr) => {
     const game = JSON.parse(xhr.response);
     addPlayers(game);
+    updateStatus(game);
   });
 };
 
@@ -45,7 +57,7 @@ const main = () => {
 
   setInterval(() => {
     updateLobby();
-  }, 100);
+  }, 500);
 };
 
 window.onload = main;
