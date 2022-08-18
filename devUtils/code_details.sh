58e4a06e42f21093d826eb21a4b1663e039844dc
chhavid <99489321+chhavid@users.noreply.github.com>
2022-08-18 22:50:35 +0530

    [#43] Chhavi -- Added pug files in code report

--- devUtils/code_details.sh
+++ devUtils/code_details.sh
@@ -13,22 +13,24 @@ function sum(){
 # Number of src files
 num_of_frontend_js_files=`ls ./public/js/*.js | wc -l`
 num_of_backend_js_files=`ls ./src/*/*.js ./src/*.js | wc -l`
+num_of_pug_files=`ls ./views/*.pug | wc -l`
 num_of_html_files=`ls ./private/*.html | wc -l`
 num_of_css_files=`ls ./public/css/*.css | wc -l`
 num_of_shell_files=`ls ./bin/*.sh ./devUtils/*.sh | wc -l`
 
-count_of_files="${num_of_frontend_js_files} ${num_of_backend_js_files} ${num_of_html_files} ${num_of_css_files} ${num_of_shell_files}"
+count_of_files="${num_of_frontend_js_files} ${num_of_backend_js_files} ${num_of_pug_files} ${num_of_html_files} ${num_of_css_files} ${num_of_shell_files}"
 
 total_src_files=`sum "${count_of_files}"`
 
 # Number of lines of src code
 num_of_frontend_js_lines=`cat ./public/js/*.js | wc -l`
 num_of_backend_js_lines=`cat ./src/*/*.js ./src/*.js | wc -l`
+num_of_pug_lines=`cat ./views/*.pug | wc -l`
 num_of_html_lines=`cat ./private/*.html | wc -l`
 num_of_css_lines=`cat public/css/*.css | wc -l`
 num_of_shell_lines=`cat ./bin/*.sh ./devUtils/*.sh | wc -l`
 
-count_of_src_lines="${num_of_frontend_js_lines} ${num_of_backend_js_lines} ${num_of_html_lines} ${num_of_css_lines} ${num_of_shell_lines}"
+count_of_src_lines="${num_of_frontend_js_lines} ${num_of_backend_js_lines} ${num_of_pug_lines} ${num_of_html_lines} ${num_of_css_lines} ${num_of_shell_lines}"
 
 total_lines_of_src_code=`sum "${count_of_src_lines}"`
 
@@ -42,6 +44,7 @@ echo -e "\n-----SOURCE CODE-----"
 echo -e "\n--Number of Files--"
 echo -e "Javascript (Front-End):"${num_of_frontend_js_files}
 echo -e "Javascript (Back-End):"${num_of_backend_js_files} 
+echo -e "PUG:"${num_of_pug_files}
 echo -e "HTML:"${num_of_html_files}
 echo -e "CSS:"${num_of_css_files}
 echo -e "Shell:"${num_of_shell_files}
@@ -50,6 +53,7 @@ echo -e "Total:"${total_src_files}
 echo -e "\n--Number of Lines of Code--"
 echo -e "Javascript (Front-End):"${num_of_frontend_js_lines} 
 echo -e "Javascript (Back-End):"${num_of_backend_js_lines}
+echo -e "PUG:"${num_of_pug_lines}
 echo -e "HTML:"${num_of_html_lines}
 echo -e "CSS:"${num_of_css_lines}
 echo -e "Shell:"${num_of_shell_lines}
